9f757cbdf3f4374e27178f2e4757aaba32d73cc9
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-18 10:55:49 +0900

    HTTP request mapping  chapter 3 Headers and parameters (헤더와 매개변수 )
      - 헤더에 요청

--- src/main/java/me/whiteship/demowebmvc/SampleController.java
+++ src/main/java/me/whiteship/demowebmvc/SampleController.java
@@ -1,5 +1,6 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -8,9 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping
 public class SampleController {
 
-    @RequestMapping(value = "/hello",
-                    consumes = MediaType.APPLICATION_JSON_VALUE,
-                    produces = MediaType.TEXT_PLAIN_VALUE)
+    @RequestMapping(value = "/hello",headers = HttpHeaders.FROM)
     @ResponseBody
     public String hello() {
         return "hello" ;
