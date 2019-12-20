c0db5b8e209062f0321e8e766854825313fbd582
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-20 23:01:36 +0900

    Handler methods 8  (핸들러 메소드 8부)
       @SessionAttributes
    
      - 모델 정보를 HTTP 세션에 저장해주는 애노테이션 : HttpSession을 직접 사용함
                                                  * @Test 사용
                                                       - 세션 정보를 디버그 모드로 봄

--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -29,12 +30,14 @@ public class SampleControllerTest {
 
     @Test
     public void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        MockHttpServletRequest request = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("/events/form"))
                 .andExpect(model().attributeExists("event"))
-                .andExpect(request().sessionAttribute("event",notNullValue()))
-        ;
+                .andExpect(request().sessionAttribute("event", notNullValue()))
+                .andReturn().getRequest();
+        Object event = request.getSession().getAttribute("event");
+        System.out.println(event);
     }
 
     @Test
