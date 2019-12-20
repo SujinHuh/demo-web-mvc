c5c9d3ec6d2c1d4347e5e27ce4837fe2a228924b
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-20 21:35:35 +0900

    Handler methods 7  (핸들러 메소드 7부)
      폼 서브밋 (에러 처리)
    
      - 바인딩 에러 발생 시 Model에 담기는 정보 : Event
                                             BindingResult.event

--- src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/whiteship/demowebmvc/SampleControllerTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,10 +37,13 @@ public class SampleControllerTest {
 
     @Test
     public void eventForm() throws Exception {
-        mockMvc.perform(get("/events/form"))
+        ResultActions result = mockMvc.perform(get("/events/form"))
                 .andDo(print())
                 .andExpect(view().name("/events/form"))
-                .andExpect(model().attributeExists("event"))
-        ;
+                .andExpect(model().attributeExists("event"));
+        ModelAndView modelAndView = result.andReturn().getModelAndView();
+        Map<String, Object> model = modelAndView.getModel();
+        System.out.println(model.size());
+
     }
 }
