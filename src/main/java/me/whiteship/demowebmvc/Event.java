9d2a6fad8ef39a7810e0b4d92347044d327ae059
수진저장소 <43295876+SujinHuh@users.noreply.github.com>
2019-12-23 23:45:02 +0900

    DataBinder: @InitBinder
     - 포매터 설정 : webDataBinder.addCustomFormatter();
    
                    * if (bindingResult.hasErrors()) , 디버거로 확인

--- src/main/java/me/whiteship/demowebmvc/Event.java
+++ src/main/java/me/whiteship/demowebmvc/Event.java
@@ -1,7 +1,11 @@
 package me.whiteship.demowebmvc;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Event {
 
@@ -13,6 +17,9 @@ public class Event {
 
     @Min(0)
     private Integer limit;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate startDate;
 
     public Integer getId() {
         return id;
@@ -36,5 +43,13 @@ public class Event {
 
     public void setLimit(Integer limit) {
         this.limit = limit;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
     }
 }
